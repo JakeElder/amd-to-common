7447763050bbc194d86d0e031b77be11d4dd221f
Jake Elder <jake@jpeg.me.uk>
2016-08-05 14:06:06 +0100

    CLI shell expansion

--- src/cli.js
+++ src/cli.js
@@ -1,27 +1,7 @@
 'use strict';
 
-var _ = require('underscore');
-var args = require('minimist')(process.argv.slice(2));
-var glob = require('glob-all');
-var path = require('path');
-var fs = require('fs');
-var AMDToCommon = require('./amd-to-common');
+const AMDToCommon = require('./amd-to-common');
+const files = process.argv.slice(2).filter(f => !/\/(vendor|misc)\//.test(f));
+const converter = new AMDToCommon({ files });
 
-function getFileGlobString(arg){
-  var stat = fs.lstatSync(arg);
-  if(stat.isDirectory()){
-    return path.join(arg, '**/*.js');
-  }
-  return arg;
-}
-
-var directory = _.first(args._) || './';
-var allJSFiles = getFileGlobString(directory);
-var excludedPattern = '!' + args.exclude;
-
-glob([allJSFiles, excludedPattern], {}, function(error, files){
-  var converter = new AMDToCommon({
-    files: files
-  });
-  converter.analyse();
-});
+converter.analyse();
